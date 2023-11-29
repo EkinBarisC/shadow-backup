58b8172e7477b11c3fc8776223a108ca1bae41cc
EkinBarisC <ekinbariscakir@gmail.com>
2023-11-30 00:42:54 +0300

    feat: show snapshot info

--- ViewModels/Pages/BackupViewModel.cs
+++ ViewModels/Pages/BackupViewModel.cs
@@ -4,6 +4,7 @@
 // All Rights Reserved.
 
 using Alphaleonis.Win32.Filesystem;
+using Alphaleonis.Win32.Vss;
 using Back_It_Up.Models;
 using Back_It_Up.ViewModels.Windows;
 using Back_It_Up.Views.Pages;
@@ -33,25 +34,43 @@ namespace Back_It_Up.ViewModels.Pages
         }
         private void PerformBackup()
         {
-            string source_file = @"C:\Users\ekin1\OneDrive\Documents\backup tool files\texts\text 1.txt";
-            string backup_root = @"C:\Users\ekin1\OneDrive\Documents\backup tool files\backups";
-            string backup_path = Path.Combine(backup_root, Path.GetFileName(source_file));
+            //string source_file = @"C:\Users\ekin1\OneDrive\Documents\backup tool files\texts\text 1.txt";
+            //string backup_root = @"C:\Users\ekin1\OneDrive\Documents\backup tool files\backups";
+            //string backup_path = Path.Combine(backup_root, Path.GetFileName(source_file));
 
-            // Initialize the shadow copy subsystem.
-            using (VssBackup vss = new VssBackup())
+            //// Initialize the shadow copy subsystem.
+            //using (VssBackup vss = new VssBackup())
+            //{
+            //    vss.Setup(Path.GetPathRoot(source_file));
+            //    string snap_path = vss.GetSnapshotPath(source_file);
+
+            //    // Here we use the AlphaFS library to make the copy.
+            //    File.Copy(snap_path, backup_path);
+            //}
+
+            // enumerate snapshots in the system and show info about them
+            IVssFactory vssImplementation = VssFactoryProvider.Default.GetVssFactory();
+            using (IVssBackupComponents backup = vssImplementation.CreateVssBackupComponents())
             {
-                vss.Setup(Path.GetPathRoot(source_file));
-                string snap_path = vss.GetSnapshotPath(source_file);
+                backup.InitializeForBackup(null);
 
-                // Here we use the AlphaFS library to make the copy.
-                File.Copy(snap_path, backup_path);
+                backup.SetContext(VssSnapshotContext.All);
+
+                foreach (VssSnapshotProperties prop in backup.QuerySnapshots())
+                {
+
+                    System.Diagnostics.Debug.WriteLine("Snapshot ID: {0:B}", prop.SnapshotId);
+                    System.Diagnostics.Debug.WriteLine("Snapshot Set ID: {0:B}", prop.SnapshotSetId);
+                    System.Diagnostics.Debug.WriteLine("Original Volume Name: {0}", prop.OriginalVolumeName);
+                }
             }
+
         }
 
 
         private void OpenSourceExplorer()
         {
-        _navigationService.Navigate(typeof(SettingsPage));
+        _navigationService.Navigate(typeof(SourceExplorerPage));
         }
 
     }
